8aeb4a5434c25e46a4fcfa6bdd408c3665dc08be
Joseph W. Breu <breu@breu.org>
2012-04-20 12:07:06 -0700

    fix search

--- recipes/volume.rb
+++ recipes/volume.rb
@@ -38,8 +38,8 @@ if Chef::Config[:solo]
   Chef::Log.warn("This recipe uses search. Chef Solo does not support search.")
 else
   # Lookup keystone api ip address
-  keystone = search(:node, "recipe:keystone\\:\\:server and chef_environment:#{node.chef_environment}")
-  if keystone[0].length > 0
+  keystone, something, result_count = Chef::Search::Query.new.search(:node, "roles:keystone AND chef_environment:#{node.chef_environment}")
+  if result_count > 0
     Chef::Log.info("Using Keystone attributes from SEARCH")
     keystone_api_ip = keystone[0]['keystone']['api_ipaddress']
     keystone_admin_port = keystone[0]['keystone']['admin_port']
