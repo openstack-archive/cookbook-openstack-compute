2616d09bcc4b355dd031d6827899cb46f406e8b5
Darren Birkett <darren.birkett@gmail.com>
2012-04-26 13:06:47 +0100

    bring nova attrs into nova namespace
    - edited attributes
    - updated readme
    - updated relevant recipes to use new attrs
    - updated cookbook version
    - updated libvirt template

--- attributes/default.rb
+++ attributes/default.rb
@@ -14,38 +14,35 @@ default["nova"]["ec2"]["adminURL"] = ""
 default["nova"]["ec2"]["internalURL"] = ""
 default["nova"]["ec2"]["publicURL"] = ""
 
-# TODO: This needs to be moved into the nova namespace
-default["volume"]["api_port"] = 8776
-default["volume"]["ipaddress"] = node["controller_ipaddress"]
-default["volume"]["adminURL"] = "http://#{default["controller_ipaddress"]}:#{default["volume"]["api_port"]}/v1"
-default["volume"]["internalURL"] = default["volume"]["adminURL"]
-default["volume"]["publicURL"] = default["volume"]["adminURL"]
+default["nova"]["volume"]["api_port"] = 8776
+default["nova"]["volume"]["ipaddress"] = node["controller_ipaddress"]
+default["nova"]["volume"]["adminURL"] = "http://#{default["controller_ipaddress"]}:#{default["nova"]["volume"]["api_port"]}/v1"
+default["nova"]["volume"]["internalURL"] = default["nova"]["volume"]["adminURL"]
+default["nova"]["volume"]["publicURL"] = default["nova"]["volume"]["adminURL"]
 
 # TODO: This needs to be moved into the nova namespace
-default["public"]["label"] = "public"
-default["public"]["ipv4_cidr"] = "192.168.100.0/24"
-default["public"]["num_networks"] = "1"
-default["public"]["network_size"] = "255"
-default["public"]["bridge"] = "br100"
-default["public"]["bridge_dev"] = "eth2"
-default["public"]["dns1"] = "8.8.8.8"
-default["public"]["dns2"] = "8.8.4.4"
+default["nova"]["network"]["public"]["label"] = "public"
+default["nova"]["network"]["public"]["ipv4_cidr"] = "192.168.100.0/24"
+default["nova"]["network"]["public"]["num_networks"] = "1"
+default["nova"]["network"]["public"]["network_size"] = "255"
+default["nova"]["network"]["public"]["bridge"] = "br100"
+default["nova"]["network"]["public"]["bridge_dev"] = "eth2"
+default["nova"]["network"]["public"]["dns1"] = "8.8.8.8"
+default["nova"]["network"]["public"]["dns2"] = "8.8.4.4"
 
 # TODO: This needs to be moved into the nova namespace
-default["private"]["label"] = "private"
-default["private"]["ipv4_cidr"] = "192.168.200.0/24"
-default["private"]["num_networks"] = "1"
-default["private"]["network_size"] = "255"
-default["private"]["bridge"] = "br200"
-default["private"]["bridge_dev"] = "eth3"
+default["nova"]["network"]["private"]["label"] = "private"
+default["nova"]["network"]["private"]["ipv4_cidr"] = "192.168.200.0/24"
+default["nova"]["network"]["private"]["num_networks"] = "1"
+default["nova"]["network"]["private"]["network_size"] = "255"
+default["nova"]["network"]["private"]["bridge"] = "br200"
+default["nova"]["network"]["private"]["bridge_dev"] = "eth3"
 
 default["controller_ipaddress"] = node["ipaddress"]
 
-# TODO: This needs to be moved into the nova namespace
-default["virt_type"] = "kvm"
-
-default["libvirt"]["auth_tcp"] = "none"
-default["libvirt"]["ssh"]["private_key"] = "-----BEGIN DSA PRIVATE KEY-----
+default["nova"]["libvirt"]["virt_type"] = "kvm"
+default["nova"]["libvirt"]["auth_tcp"] = "none"
+default["nova"]["libvirt"]["ssh"]["private_key"] = "-----BEGIN DSA PRIVATE KEY-----
 MIIBvAIBAAKBgQDUIz3rg0afavOwNeTJL/112U/l4B08kzZVx+QcflxllpW4sn/f
 c+j+BeQ/sm2oW67vY9O/1GbN3FIN7Um3p0F9ycpfXpEiwk4UYneJtXFNhlu9rSrK
 hWsEWENoKrCFhZ4Zuu8ads0DCMkU/ErumXMvJZQpSe+8CfguYSMbXvkYhQIVAPzY
@@ -57,4 +54,4 @@ CYiYhwoK8RM+z0TNXjBIWXpHwAqX5kFhg/xPySxWS58GePmPOXDbFEYq5FRWTx47
 sQqRmVHmlZZ9AhsRfs65g4LlgJyBlWPeZ0xsfShYHKLKg5RrOGn90egCFQCcok5v
 1TpUNWQC3NPFkwWHkp1zrg==
 -----END DSA PRIVATE KEY-----"
-default["libvirt"]["ssh"]["public_key"] = "ssh-dss AAAAB3NzaC1kc3MAAACBANQjPeuDRp9q87A15Mkv/XXZT+XgHTyTNlXH5Bx+XGWWlbiyf99z6P4F5D+ybahbru9j07/UZs3cUg3tSbenQX3Jyl9ekSLCThRid4m1cU2GW72tKsqFawRYQ2gqsIWFnhm67xp2zQMIyRT8Su6Zcy8llClJ77wJ+C5hIxte+RiFAAAAFQD82LMjyqjmt7HLIS6sD7pWReuJ2QAAAIAFjrS6pwVInxqA9jZhmYtOxD86tVtSyfBP5ER5eOpqh9S92UrjeRYbDwjW/0GuH7faYVxi7YvSLO/dm8f3B3Y4SJmcfG1kSiMAsF0owVFsLw8sLYibPfK83ASj5mSBKjkvM/2dJzI78kONvI4kA8SwK0pHGa2HhcFy5jmU8C1OUQAAAIEAtFtXDovPhgvLGhFrRZjBzp3HREWW1tihsWZA4qIFib+Rd+/s3lWGCYiYhwoK8RM+z0TNXjBIWXpHwAqX5kFhg/xPySxWS58GePmPOXDbFEYq5FRWTx47sQqRmVHmlZZ9AhsRfs65g4LlgJyBlWPeZ0xsfShYHKLKg5RrOGn90eg= root@example.com"
+default["nova"]["libvirt"]["ssh"]["public_key"] = "ssh-dss AAAAB3NzaC1kc3MAAACBANQjPeuDRp9q87A15Mkv/XXZT+XgHTyTNlXH5Bx+XGWWlbiyf99z6P4F5D+ybahbru9j07/UZs3cUg3tSbenQX3Jyl9ekSLCThRid4m1cU2GW72tKsqFawRYQ2gqsIWFnhm67xp2zQMIyRT8Su6Zcy8llClJ77wJ+C5hIxte+RiFAAAAFQD82LMjyqjmt7HLIS6sD7pWReuJ2QAAAIAFjrS6pwVInxqA9jZhmYtOxD86tVtSyfBP5ER5eOpqh9S92UrjeRYbDwjW/0GuH7faYVxi7YvSLO/dm8f3B3Y4SJmcfG1kSiMAsF0owVFsLw8sLYibPfK83ASj5mSBKjkvM/2dJzI78kONvI4kA8SwK0pHGa2HhcFy5jmU8C1OUQAAAIEAtFtXDovPhgvLGhFrRZjBzp3HREWW1tihsWZA4qIFib+Rd+/s3lWGCYiYhwoK8RM+z0TNXjBIWXpHwAqX5kFhg/xPySxWS58GePmPOXDbFEYq5FRWTx47sQqRmVHmlZZ9AhsRfs65g4LlgJyBlWPeZ0xsfShYHKLKg5RrOGn90eg= root@example.com"
--- recipes/api-os-volume.rb
+++ recipes/api-os-volume.rb
@@ -60,13 +60,13 @@ else
   # Lookup keystone api ip address
   keystone, something, arbitrary_value = Chef::Search::Query.new.search(:node, "roles:keystone AND chef_environment:#{node.chef_environment}")
   if keystone.length > 0
-    Chef::Log.info("api-os-volume/keystone: using search")
+    Chef::Log.info("nova::api-os-volume/keystone: using search")
     keystone_api_ip = keystone[0]['keystone']['api_ipaddress']
     keystone_service_port = keystone[0]['keystone']['service_port']
     keystone_admin_port = keystone[0]['keystone']['admin_port']
     keystone_admin_token = keystone[0]['keystone']['admin_token']
   else
-    Chef::Log.info("api-os-volume/keystone: NOT using search")
+    Chef::Log.info("nova::api-os-volume/keystone: NOT using search")
     keystone_api_ip = node['keystone']['api_ipaddress']
     keystone_service_port = node['keystone']['service_port']
     keystone_admin_port = node['keystone']['admin_port']
