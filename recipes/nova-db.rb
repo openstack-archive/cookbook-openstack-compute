e2483aca5f1cdec6d7c69a75e9b9394da61fe183
rpedde <ron@pedde.com>
2012-06-07 12:32:28 -0500

    process monitoring for mysql

---
+++ recipes/nova-db.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook Name:: nova
+# Recipe:: nova-db
+#
+# Copyright 2009, Rackspace Hosting, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "nova::nova-common"
+include_recipe "monit::server"
+include_recipe "mysql::server"
+
+platform_options = node["nova"]["platform"]
+
+monit_procmon "mysqld" do
+  process_name "mysqld"
+  start_cmd platform_options["monit_commands"]["mysqld"]["start"]
+  stop_cmd platform_options["monit_commands"]["mysqld"]["stop"]
+end
