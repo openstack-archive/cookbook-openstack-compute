3d35fc147c086513013f35690a05b0c1fdf6fa2d
Jason Cannavale <jason.cannavale@rackspace.com>
2012-06-20 15:55:47 -0500

    Adding syslog attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -41,6 +41,10 @@ default["nova"]["services"]["volume"]["network"] = "public"
 default["nova"]["services"]["volume"]["port"] = 8776
 default["nova"]["services"]["volume"]["path"] = "/v1/%(tenant_id)s"
 
+# Logging stuff
+default["nova"]["syslog"]["use"] = True
+default["nova"]["syslog"]["facility"] = "LOG_LOCAL1"
+
 # can this be wedged into the "api" endpoint?
 default["nova"]["compute"]["region"] = "RegionOne"
 
