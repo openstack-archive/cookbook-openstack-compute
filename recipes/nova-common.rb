82c8e5d15bda3277e04d81daf94ea44bb5501991
Jason Cannavale <jason.cannavale@rackspace.com>
2012-06-20 14:11:29 -0500

    Create nova-rsyslog recipe to configure syslog facility and log location, called by nova-common

--- recipes/nova-common.rb
+++ recipes/nova-common.rb
@@ -16,6 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+include_recipe "nova::nova-rsyslog"
 
 platform_options = node["nova"]["platform"]
 
