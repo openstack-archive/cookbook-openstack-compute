92ffcb167f061b064161fb67795edc1478737b07
rcbjenkins <rcb-deploy@lists.rackspace.com>
2012-07-12 17:06:27 -0500

    End of sprint version bump.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Rackspace Hosting, Inc."
 license           "Apache 2.0"
 description       "Installs and configures Openstack"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.7"
+version           "1.0.8"
 # recipe            "mysql", "Includes the client recipe to configure a client"
 # recipe            "mysql::client", "Installs packages required for mysql clients using run_action magic"
 
