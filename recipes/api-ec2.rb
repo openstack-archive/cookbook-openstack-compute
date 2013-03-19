da17ee847c3cbb426dd53efcb3037b95eb978a01
Jay Pipes <jaypipes@gmail.com>
2013-03-19 18:01:22 -0400

    Separate out Keystone endpoint registration into separate recipe

--- recipes/api-ec2.rb
+++ recipes/api-ec2.rb
@@ -17,8 +17,6 @@
 # limitations under the License.
 #
 
-require "uri"
-
 class ::Chef::Recipe
   include ::Openstack
 end
@@ -55,73 +53,8 @@ service "nova-api-ec2" do
   action :enable
 end
 
-service_pass = service_password "nova"
-
 identity_admin_endpoint = endpoint "identity-admin"
-keystone_service_role = node["nova"]["keystone_service_chef_role"]
-keystone = config_by_role keystone_service_role, "keystone"
-
-ec2_admin_endpoint = endpoint "compute-ec2-admin"
-ec2_public_endpoint = endpoint "compute-ec2-api"
-
-bootstrap_token = secret "secrets", "keystone_bootstrap_token"
-auth_uri = ::URI.decode identity_admin_endpoint.to_s
-
-# Register Service Tenant
-keystone_register "Register Service Tenant" do
-  auth_uri auth_uri
-  bootstrap_token bootstrap_token
-  tenant_name node["nova"]["service_tenant_name"]
-  tenant_description "Service Tenant"
-
-  action :create_tenant
-end
-
-# Register Service User
-keystone_register "Register Service User" do
-  auth_uri auth_uri
-  bootstrap_token bootstrap_token
-  tenant_name node["nova"]["service_tenant_name"]
-  user_name node["nova"]["service_user"]
-  user_pass service_pass
-
-  action :create_user
-end
-
-# Grant Admin role to Service User for Service Tenant
-keystone_register "Grant 'admin' Role to Service User for Service Tenant" do
-  auth_uri auth_uri
-  bootstrap_token bootstrap_token
-  tenant_name node["nova"]["service_tenant_name"]
-  user_name node["nova"]["service_user"]
-  role_name node["nova"]["service_role"]
-
-  action :grant_role
-end
-
-# Register EC2 Service
-keystone_register "Register EC2 Service" do
-  auth_uri auth_uri
-  bootstrap_token bootstrap_token
-  service_name "ec2"
-  service_type "ec2"
-  service_description "EC2 Compatibility Layer"
-
-  action :create_service
-end
-
-# Register EC2 Endpoint
-keystone_register "Register Compute Endpoint" do
-  auth_uri auth_uri
-  bootstrap_token bootstrap_token
-  service_type "ec2"
-  endpoint_region node["nova"]["region"]
-  endpoint_adminurl ::URI.decode ec2_admin_endpoint.to_s
-  endpoint_internalurl ::URI.decode ec2_public_endpoint.to_s
-  endpoint_publicurl ::URI.decode ec2_public_endpoint.to_s
-
-  action :create_endpoint
-end
+service_pass = service_password "nova"
 
 template "/etc/nova/api-paste.ini" do
   source "api-paste.ini.erb"
