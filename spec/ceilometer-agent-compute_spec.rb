eca54e7692c7bd1f8a72f045b6447dfa3cf47c13
Ionuț Arțăriși <iartarisi@suse.cz>
2013-06-04 18:28:55 +0200

    add tests for ceilometer agent-compute recipe
    Change-Id: I645835ad844cc1baa371b93e4f759d88237ffd82

---
+++ spec/ceilometer-agent-compute_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe "openstack-compute::ceilometer-agent-compute" do
+  describe "ubuntu" do
+    before do
+      compute_stubs
+      @chef_run = ::ChefSpec::ChefRunner.new ::UBUNTU_OPTS
+      @chef_run.converge "openstack-compute::ceilometer-agent-compute"
+    end
+
+    expect_runs_ceilometer_common_recipe
+
+    it "starts ceilometer-agent-compute service" do
+      expect(@chef_run).to start_service("ceilometer-agent-compute")
+    end
+  end
+end
