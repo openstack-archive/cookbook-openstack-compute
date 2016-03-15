541c8ab7512b28a51441990c496a306c3ef372ba
Jan Klare <j.klare@x-ion.de>
2016-03-15 16:10:46 +0100

    Fix bootstrap.sh
    On recent test machines, curl isn't available anymore, so use wget
    instead. Piping directly into bash isn't that great anyway.
    
    Change-Id: I5d6b5480cf8dc789848c7a0f54b88646500c94d0

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,5 @@
 ## This script is for installing all the needed packages on trusty to run the chef tests with 'chef exec rake'.
 ## It relies on the common bootstrap.sh from openstack/cookbook-openstack-common for installing common dependencies.
 
-curl https://raw.githubusercontent.com/openstack/cookbook-openstack-common/stable/liberty/bootstrap.sh \
-  --retry 3 \
-  --silent \
-  --show-error \
-  | /bin/bash -x
+wget -nv -t 3 -O common-bootstrap.sh https://raw.githubusercontent.com/openstack/cookbook-openstack-common/stable/liberty/bootstrap.sh
+/bin/bash -x common-bootstrap.sh
