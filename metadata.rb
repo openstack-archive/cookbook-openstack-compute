64c25ea5fcd81daa78e05c0e85c5efcc2b20b52e
Jan Klare <j.klare@cloudbau.de>
2016-05-31 10:21:17 +0200

    initial commit for the newton development cycle
    Change-Id: Iedc63af610d655b1d390890f466159ef77a17da9

--- metadata.rb
+++ metadata.rb
@@ -4,15 +4,15 @@ maintainer_email 'openstack-dev@lists.openstack.org'
 license 'Apache 2.0'
 description 'The OpenStack Compute service Nova.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '13.0.0'
+version '14.0.0'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
 end
 
 depends 'ceph', '>= 0.8.1'
-depends 'openstack-common', '>= 13.0.0'
-depends 'openstack-identity', '>= 13.0.0'
-depends 'openstack-image', '>= 13.0.0'
-depends 'openstack-network', '>= 13.0.0'
+depends 'openstack-common', '>= 14.0.0'
+depends 'openstack-identity', '>= 14.0.0'
+depends 'openstack-image', '>= 14.0.0'
+depends 'openstack-network', '>= 14.0.0'
 depends 'python', '~> 1.4.6'
