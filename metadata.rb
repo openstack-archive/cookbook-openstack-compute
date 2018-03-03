0bef2dc3fcc5e0d360ef9d4f71fd7d188fa7b29f
Samuel Cassiba <s@cassiba.com>
2018-03-02 17:58:10 -0800

    starting queens development patch and use git.openstack.org
    * use git.openstack.org instead of github for berks dependency
    resolution
    
    Depends-On: https://review.openstack.org/549345
    Depends-On: https://review.openstack.org/549346
    Depends-On: https://review.openstack.org/549348
    Change-Id: Ie18c080e001070bb081e30ec0633f3c25a5f2b55

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ issues_url       'https://launchpad.net/openstack-chef' if respond_to?(:issues_u
 source_url       'https://github.com/openstack/cookbook-openstack-compute' if respond_to?(:source_url)
 license          'Apache-2.0'
 description      'The OpenStack Compute service Nova.'
-version          '16.0.0'
+version          '17.0.0'
 
 chef_version '>= 12.5' if respond_to?(:chef_version)
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
@@ -30,8 +30,8 @@ recipe 'openstack-compute::vncproxy', 'Installs/Configures nova vnc proxy'
 end
 
 depends 'ceph'
-depends 'openstack-common', '>= 16.0.0'
-depends 'openstack-identity', '>= 16.0.0'
-depends 'openstack-image', '>= 16.0.0'
-depends 'openstack-network', '>= 16.0.0'
+depends 'openstack-common', '>= 17.0.0'
+depends 'openstack-identity', '>= 17.0.0'
+depends 'openstack-image', '>= 17.0.0'
+depends 'openstack-network', '>= 17.0.0'
 depends 'openstackclient'
