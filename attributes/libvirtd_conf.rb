ad3d1c3c652a2cbaa7aa6dd8d8cff347095c5909
Jens Harbott <j.harbott@x-ion.de>
2018-04-23 11:33:35 +0000

    Refactor libvirtd.conf creation
    Replace the hardcoded bloated template with a variant of our standard
    template that only includes the values that are really needed.
    
    This will allow deployments to override the default values and for
    example deploy libvirtd with TLS authentication according to [0].
    
    [0] https://wiki.openstack.org/wiki/OSSN/OSSN-0007
    
    Change-Id: Ib7b305670f525bbf975ec33ed070e2d960347ef4

--- attributes/libvirtd_conf.rb
+++ attributes/libvirtd_conf.rb
@@ -1,16 +1,18 @@
 # libvirtd_opts used in template for /etc/default/libvirt-bin
 default['openstack']['compute']['libvirt']['libvirtd_opts'] = '-l'
 
-default['openstack']['compute']['libvirt']['auth_tcp'] = 'none'
-# libvirt.max_clients (default: 20)
-default['openstack']['compute']['libvirt']['max_clients'] = 20
-# libvirt.max_workers (default: 20)
-default['openstack']['compute']['libvirt']['max_workers'] = 20
-# libvirt.max_requests (default: 20)
-default['openstack']['compute']['libvirt']['max_requests'] = 20
-# libvirt.max_client_requests (default: 5)
-default['openstack']['compute']['libvirt']['max_client_requests'] = 5
 default['openstack']['compute']['libvirt']['group'] = 'libvirt'
-default['openstack']['compute']['libvirt']['unix_sock_rw_perms'] = '0770'
-default['openstack']['compute']['libvirt']['libvirt_inject_key'] = true
 default['openstack']['compute']['libvirt']['volume_backend'] = nil
+
+default['openstack']['compute']['libvirt']['conf'].tap do |conf|
+  conf['listen_tls'] = '0'
+  conf['listen_tcp'] = '1'
+  conf['unix_sock_rw_perms'] = '"0770"'
+  conf['auth_unix_ro'] = '"none"'
+  conf['auth_unix_rw'] = '"none"'
+  conf['auth_tcp'] = '"none"'
+  conf['max_clients'] = '20'
+  conf['max_workers'] = '20'
+  conf['max_requests'] = '20'
+  conf['max_client_requests'] = '5'
+end
